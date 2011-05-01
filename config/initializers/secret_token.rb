240046f06b2913871f627d9004c8cd05c2b6a053
Paul Jones <shmiko@gmail.com>
2011-05-01 14:43:42 +1000

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shmik::Application.config.secret_token = 'd4595a962ade240c72e2a5a5f013143691683022b717146d5c9f9125ca553a70c0b80e33fe713095b346252ae065fbbc046bcd0ef9f9e26dde2708315a542d79'
---
+++ shmik/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shmik::Application.config.secret_token = '02e78ab7ff552907d60674ec383ee7ed43bf15b870d3348e53729b56d95e79c4a96dd7d4a79c23cb6cbea6ff175d4c8c438f9177c4d1d8102218cd147633cf52'
